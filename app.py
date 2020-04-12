025d2033f2dd29b8bed5945d84868fbe4ae5c3ce
syedmrizwan <syed.m.rizwan@outlook.com>
2020-04-12 23:34:38 +0500

    called synchrous cadence workflow

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import subprocess
 
+# calling asynchronouslly
 process = subprocess.Popen(
     ['sudo', 'docker', 'run', '--network=host', '--rm', 'ubercadence/cli:master', '--domain', 'test-domain', 'workflow',
      'start', '--wt', 'github.com/syedmrizwan/orchestrator/src/workflows.DemoWorkflow', '--tl', 'helloWorldGroup',
@@ -10,3 +11,14 @@ process = subprocess.Popen(
 stdout, stderr = process.communicate()
 
 print(stdout, stderr)
+
+
+# calling synchronouslly
+
+process = subprocess.Popen(['sudo', 'docker', 'run', '--network=host', '--rm', 'ubercadence/cli:master', '--domain', 'test-domain', 'workflow', 'run', '--tl', 'helloWorldGroup', '--wt', 'github.com/syedmrizwan/orchestrator/src/workflows.DemoWorkflow', '--et', '300'
+                            ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, errors = process.communicate(
+    input="Hello from the other side!")
+process.wait()
+print(output)
+print(errors)
