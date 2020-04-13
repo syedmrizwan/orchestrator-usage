1c6a2f4d3ed2dcd3136ee02eeae88ad1af2cdb1a
syedmrizwan <syed.m.rizwan@outlook.com>
2020-04-13 16:13:50 +0500

    Parse cadence workflow output

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import shlex
 import subprocess
+import json
 
 # calling asynchronouslly
 process = subprocess.Popen(
@@ -15,10 +17,16 @@ print(stdout, stderr)
 
 # calling synchronouslly
 
-process = subprocess.Popen(['sudo', 'docker', 'run', '--network=host', '--rm', 'ubercadence/cli:master', '--domain', 'test-domain', 'workflow', 'run', '--tl', 'helloWorldGroup', '--wt', 'github.com/syedmrizwan/orchestrator/src/workflows.DemoWorkflow', '--et', '300'
-                            ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, errors = process.communicate(
-    input="Hello from the other side!")
-process.wait()
-print(output)
-print(errors)
+command = 'sudo docker run --network=host --rm ubercadence/cli:master --domain test-domain workflow run --tl helloWorldGroup --wt github.com/syedmrizwan/orchestrator/src/workflows.DemoWorkflow --et 300'
+args = shlex.split(command)
+process = subprocess.Popen(args, stdin=subprocess.PIPE,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# stdout, stderr = process.communicate()
+# process.wait()
+# print(stdout)
+# print(stderr)
+for line in process.stdout:
+    if 'Output:' in line:
+        temp = line.rstrip().replace("Output:", "").strip()
+        temp = json.loads(temp)
+        print "test:", temp['value2']
